295835bd851bdba7dfc6ca82e465f184ae87bfd4
umakant030126 <umakant030126@gmail.com>
2026-04-24 22:13:38 +0530

    doing moretask of day 1 and day for revision

--- PythonTaskAndRoadMap.md
+++ PythonTaskAndRoadMap.md
@@ -48,11 +48,11 @@
 ---
 
 ## Day 5
-- [ ] 21. Max/min in list
-- [ ] 22. Remove duplicates
-- [ ] 23. Manual sort
-- [ ] 24. Merge lists
-- [ ] 25. Second largest
+- [✓] 21. Max/min in list
+- [✓] 22. Remove duplicates
+- [✓] 23. Manual sort
+- [✓] 24. Merge lists
+- [✓] 25. Second largest
 
 ---
 📅 DAY 1 (Strings + Basics)
---
+++ consistency/moreTaskCode/Day1And2.py
@@ -0,0 +1,32 @@
+# 📅 Day 1 – Absolute Basics
+# Print "Hello World"
+print("Hello world")
+
+# Swap two variables (without third variable)
+n1 , n2 = 10 , 5
+print(n1 , n2)
+n1 = n1 * n2
+n2 = int(n1 / n2)
+n1 = int(n1 / n2)
+print(n1 , n2)
+
+# Check if a number is even or odd
+# Find largest of 3 numbers
+# Reverse a number
+# Count digits in a number
+# Sum of digits
+# Check palindrome number
+# Find factorial using loop
+# Fibonacci series (n terms)
+#
+# 📅 Day 2 – Numbers + Logic
+# Check prime number (optimized)
+# Find all divisors of a number
+# Check perfect number
+# Convert binary to decimal
+# Convert decimal to binary
+# Find power using recursion
+# GCD of two numbers
+# LCM of two numbers
+# Armstrong number
+# Sum of natural numbers
